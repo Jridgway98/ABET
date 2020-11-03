606c16ae8d2454b48649aa75f429d2ecff237196
Joseph Ridgway <32882950+GroupTube@users.noreply.github.com>
2020-11-02 22:24:11 -0600

    Update loginPage.php

--- html/loginPage.php
+++ html/loginPage.php
@@ -4,25 +4,25 @@ include("dbconnect.php");
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-  $username = mysqli_real_escape_string($db,$_POST['username']);
-  $password = mysqli_real_escape_string($db,$_POST['password']);
+//   $username = mysqli_real_escape_string($db,$_POST['username']);
+//   $password = mysqli_real_escape_string($db,$_POST['password']);
 
-  $sql = "SELECT id FROM User WHERE username = '$username' and password = '$password'";
-  $result = mysqli_query($db, $sql);
-  $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-  $active = $row['active'];
+//   $sql = "SELECT id FROM User WHERE username = '$username' and password = '$password'";
+//   $result = mysqli_query($db, $sql);
+//   $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+//   $active = $row['active'];
 
-  $count = mysqli_num_rows($result);
+//   $count = mysqli_num_rows($result);
 
-  if($count == 1) {
-    session_register("myusername");
-    $_SESSION['login_user'] = $username;
-  }
-  else{
-    $error = "Invalid username or password."
-  }
+//   if($count == 1) {
+//     session_register("myusername");
+//     $_SESSION['login_user'] = $username;
+//   }
+//   else{
+//     $error = "Invalid username or password."
+//   }
 
-}
+// }
 ?>
 
 <!DOCTYPE html>
