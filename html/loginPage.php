a5b8776890fc79039a256a43d8886d6b98e9a161
Joseph Ridgway <joseph.ridgway@gmail.com>
2020-11-02 21:50:47 -0500

    Initial commit

---
+++ html/loginPage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" type="text/css" href="loginPage.css">
+  <script defer src="login-page.js"></script>
+</head>
+
+<body>
+  
+  <!--<div class="sidenav-left"></div>-->
+  <!--<div class="sidenav-right"></div>-->
+
+  <main id="main-holder">
+    <h1 id="login-header">WKU ABET Materials Database Login</h1>
+    
+    <div id="login-error-msg-holder">
+      <p id="login-error-msg">Invalid email <span id="error-msg-second-line">and/or password</span></p>
+    </div>
+    
+    <form id="login-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" "method="post">
+      <input type="text" name="username" id="email-field" class="login-form-field" placeholder="Username">
+      <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password">
+      <input type="submit" value="Login" id="login-form-submit">
+    </form>
+  
+  </main>
+</body>
+
+</html>
